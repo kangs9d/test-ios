cee597d766b4ba009d1027422ae0fc0caff2fcda
bakjuna <bakjuna@gmail.com>
2023-02-04 23:24:52 +0900

    Initial Commit

---
+++ test_ios/test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_iosApp.swift
+//  test_ios
+//
+//  Created by 박준하 on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
